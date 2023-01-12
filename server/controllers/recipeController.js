802d6b929f52894ecb6841eae61131a7d1de7fca
nithindaniel1 <nithindaniel2018@gmail.com>
2023-01-12 07:02:28 +0530

    recipe schema and added data

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,6 @@
 require('../models/database');
 const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
 /**
  * GET/
  * Homepage
@@ -85,3 +86,46 @@ exports.exploreCategories = async(req,res)=> {
 
 
 // insertDymmyCategoryData();
+
+
+
+
+
+
+
+async function insertDymmyRecipeData(){
+    try{
+        await Recipe.insertMany([
+            { 
+                    "name": "Recipe Name Goes Here",
+                    "description": `Recipe Description Goes Here`,
+                    "email": "recipeemail@raddy.co.uk",
+                    "ingredients": [
+                      "1 level teaspoon baking powder",
+                      "1 level teaspoon cayenne pepper",
+                      "1 level teaspoon hot smoked paprika",
+                    ],
+                    "category": "American", 
+                    "image": "southern-friend-chicken.jpg"
+                  },
+                  { 
+                    "name": "Recipe Name Goes Here",
+                    "description": `Recipe Description Goes Here`,
+                    "email": "recipeemail@raddy.co.uk",
+                    "ingredients": [
+                      "1 level teaspoon baking powder",
+                      "1 level teaspoon cayenne pepper",
+                      "1 level teaspoon hot smoked paprika",
+                    ],
+                    "category": "American", 
+                    "image": "southern-friend-chicken.jpg"
+                  },
+                ]);
+    }catch (error){
+        console.log(error);
+
+    }
+}
+
+
+insertDymmyRecipeData();
