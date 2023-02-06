a46bebbd76bf70372f7a0de51f43d22ec8184aa8
nithindaniel1 <nithindaniel2018@gmail.com>
2023-02-06 21:46:06 +0530

    explore-random

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -140,9 +140,23 @@ exports.exploreLatest = async (req,res)=> {
 
 
 
+/**
+ * GET/explore-random
+ * Explore Random as Json
+ */
 
+exports.exploreRandom = async (req,res)=> {
 
-
+  try{
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random',{title:'Cooking Blog - Explore Latest',recipe});
+      
+  }catch (error){
+      res.status(500).send({message:error.message || "Error Occured"});
+  }
+}
 
 
 
--- server/routes/recepieRoutes.js
+++ server/routes/recepieRoutes.js
@@ -11,5 +11,6 @@ router.get('/categories',recepieController.exploreCategories);
 router.get('/categories/:id',recepieController.exploreCategoriesById);
 router.post('/search',recepieController.searchRecipe);
 router.get('/explore-latest',recepieController.exploreLatest);
+router.get('/explore-random',recepieController.exploreRandom);
 
 module.exports = router;
